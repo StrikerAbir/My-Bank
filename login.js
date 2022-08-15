7de9867ba5cfe1af1496d43b52bc8244fd2fbea6
Abir Hasan <cse.170201063@gmail.com>
2022-08-16 02:48:16 +0600

    26.3 add event listener and compare email and pass

---
+++ login.js
@@ -0,0 +1,19 @@
+// step 1 : add click handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function () {
+    // step 2 :get the email address from input field
+    // alwalys remember to use .value to get text from an input field.
+    const emailField = document.getElementById('user-email')
+    const email = emailField.value;
+
+    // step 3: get password
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+    // ! Do not varify email password on the client side.
+    // step 4: varify email and password on the server side
+    if (email === 'jahanaratisha@gmail.com' && password === 'tisha loves abir') {
+        console.log('valid user');
+    } else {
+        console.log('invalid user');
+    }
+})
