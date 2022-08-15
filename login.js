6decbb34bfb6d98dae70632f1aa9fca68ea82c23
Abir Hasan <cse.170201063@gmail.com>
2022-08-16 03:16:20 +0600

    26.4 navigate new page deposite, withdraw, balance grid

--- login.js
+++ login.js
@@ -8,12 +8,15 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // step 3: get password
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
-    console.log(email, password);
+    // console.log(email, password);
+    
     // ! Do not varify email password on the client side.
     // step 4: varify email and password on the server side
     if (email === 'jahanaratisha@gmail.com' && password === 'tisha loves abir') {
         console.log('valid user');
+
     } else {
+        alert('Invalid user. Enter corret email and password');
         console.log('invalid user');
     }
 })
