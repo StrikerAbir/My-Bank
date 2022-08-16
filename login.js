da701690800186f98bde61eb0fe3f9129c081a3f
Abir Hasan <cse.170201063@gmail.com>
2022-08-16 17:36:49 +0600

    2 page linked

--- login.js
+++ login.js
@@ -13,7 +13,8 @@ document.getElementById('btn-submit').addEventListener('click', function () {
     // ! Do not varify email password on the client side.
     // step 4: varify email and password on the server side
     if (email === 'strikerabirhasan@gmail.com' && password === 'strikerabir63') {
-        console.log('valid user');
+        // console.log('valid user');
+        window.location.href = 'bank.html';
 
     } else {
         alert('Invalid user. Enter corret email and password');
